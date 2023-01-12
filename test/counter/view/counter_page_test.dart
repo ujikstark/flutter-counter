9fdc32965840529e3a39a3f0949b98226dd07895
ujikstark <ujikbauk@gmail.com>
2023-01-12 15:22:50 +0700

    initial

---
+++ test/counter/view/counter_page_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_counter/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CouterPage', () {
+    testWidgets('renders CounterView', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: CounterPage(),));
+      expect(find.byType(CounterView), findsOneWidget);
+    });
+  });
+}
---
+++ test/counter/view/counter_view_test.dart
@@ -0,0 +1,65 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockCounterCubit extends MockCubit<int> implements CounterCubit {}
+
+const _incrementButtonKey = Key('counterView_increment_floatingActionButton');
+const _decrementButtonKey = Key('counterView_decrement_floatingActionButton');
+
+void main() {
+  late CounterCubit counterCubit;
+
+  setUp(() {
+    counterCubit = MockCounterCubit();
+  });
+
+  group('CounterView', () {
+    testWidgets('renders current CounterCubit state', (tester) async {
+      when(() => counterCubit.state).thenReturn(42);
+      await tester.pumpWidget(MaterialApp(
+        home: BlocProvider.value(
+          value: counterCubit,
+          child: CounterView(),
+        ),
+      ));
+      expect(find.text('42'), findsOneWidget);
+    });
+
+    testWidgets('tapping increment button invokes increment', (tester) async {
+      when(() => counterCubit.state).thenReturn(0);
+      when(() => counterCubit.increment()).thenReturn(null);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider.value(
+            value: counterCubit,
+            child: CounterView(),
+          ),
+        ),
+      );
+      await tester.tap(find.byKey(_incrementButtonKey));
+      // assert the number of increment called
+      verify(() => counterCubit.increment()).called(1);
+    });
+
+    testWidgets('tapping decrement button invokes decrement', (tester) async {
+      when(() => counterCubit.state).thenReturn(0);
+      when(() => counterCubit.increment()).thenReturn(null);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider.value(
+            value: counterCubit,
+            child: CounterView(),
+          ),
+        ),
+      );
+      await tester.tap(find.byKey(_decrementButtonKey));
+      // assert the number of decrement called
+      verify(() => counterCubit.decrement()).called(1);
+    });
+  });
+}
